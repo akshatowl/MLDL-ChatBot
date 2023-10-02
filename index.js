d513557c903957c2bbb3d6959cf0a8e4fd56d096
akshatowl <akshatpandeyplus41@gmail.com>
2023-10-02 03:47:52 -0600

    Organize

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebas
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
-document.getElementById('loginForm').addEventListener('submit', function (e) {
+document.getElementById('loginForm').addEventListener('submit', function (e) { //fetch username and password entered in text fields of login page
     e.preventDefault();
     var username = document.getElementById('Username').value;
     var password = document.getElementById('Password').value;
 
-window.onerror = function() {
+window.onerror = function() { //function to throw error for wrong password/username
     alert('Error message: Wrong Username or Password');
     return true;
 };
-// Initialize Firebase
+// Initialize Firebase Config
 const firebaseConfig = {
     apiKey: "AIzaSyBPnrk6AHSsjpiConILj8gYbMGVrrGPw6U",
     authDomain: "mldl-chatbot.firebaseapp.com",
@@ -30,12 +30,12 @@ const firebaseConfig = {
   // Query the database to find a user with the entered username
   onValue(userRef, (snapshot) => {
       const users = snapshot.val();
-      const user = Object.values(users).find(u => u.Username === username);
+      const user = Object.values(users).find(u => u.Username === username); //Query the database to find user that has same username and password
 
       if (user) {
           // User with the entered username exists
-          if (user.Password === password) {
-              // Password matches, the user is authenticated
+          if (user.Password === password) { //compare entered password to password in database
+              
               console.log("User is authenticated");
               window.location.href = "main.html";
 
