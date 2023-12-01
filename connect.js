9f6012fa220c03e004870a952bcee3557f2939bf
akshatowl <akshatpandeyplus41@gmail.com>
2023-12-01 07:47:28 -0600

    mongodb changes

--- connect.js
+++ connect.js
@@ -1,162 +1,150 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+const chatMessages = document.getElementById('chat-messages');
+const userInput = document.getElementById('user-input');
+const unorderedConversationList = document.getElementById("unordered-conversation-list");
 
-        const firebaseConfig = {
-    apiKey: "AIzaSyBPnrk6AHSsjpiConILj8gYbMGVrrGPw6U",
-    authDomain: "mldl-chatbot.firebaseapp.com",
-    databaseURL: "https://mldl-chatbot-default-rtdb.firebaseio.com",
-    projectId: "mldl-chatbot",
-    storageBucket: "mldl-chatbot.appspot.com",
-    messagingSenderId: "988783282097",
-    appId: "1:988783282097:web:9be4075e02a5e7fed31f90",
-    measurementId: "G-1VQ6B1E6TD"
-  };
+var sessionID = new Date().getTime();
+var messageNum = 0;
+var sessionNum = 0;
+var userID = localStorage.getItem("userID");
+var sessions = [];
+var baseUrl = "http://localhost:3000"; 
 
-        const firebaseApp = initializeApp(firebaseConfig);
-        const database = getDatabase(firebaseApp);
+console.log(userID);
 
-
-        const chatMessages = document.getElementById('chat-messages');
-        const userInput = document.getElementById('user-input');
-        var sessionID = new Date().getTime();
-        var messageNum = 0;
-        var sessionNum = 0;
-        var userID = localStorage.getItem("userID");
-        var sessions = [];
-
-        console.log(userID);
-        const unorderedConversationList = document.getElementById("unordered-conversation-list");
-
-        const dataRef2 = ref(database);
-        get(child(dataRef2, "UserToMessages/" + userID)).then((snapshot) =>{
-            if(snapshot.exists()) {
-                console.log(snapshot.val());
-                sessionNum = snapshot.val().length;
-                for (let i = 0 ; i < snapshot.val().length ; i++) {
-                    sessions.push(snapshot.val()[i]);
-                    const conversationItem = document.createElement('li');
-                    conversationItem.dataset.conversation = "" + (i+1);
-                    conversationItem.appendChild(document.createTextNode("Conversation: " + (i + 1)));
-                    unorderedConversationList.appendChild(conversationItem);
-                }
+const dataRef2 = `${baseUrl}/messages/${userID}`;
+fetch(dataRef2)
+    .then(response => response.json())
+    .then(snapshot => {
+        if (snapshot.exists) {
+            console.log(snapshot.val());
+            sessionNum = snapshot.val().length;
+            for (let i = 0; i < snapshot.val().length; i++) {
+                sessions.push(snapshot.val()[i]);
+                const conversationItem = document.createElement('li');
+                conversationItem.dataset.conversation = "" + (i + 1);
+                conversationItem.appendChild(document.createTextNode("Conversation: " + (i + 1)));
+                unorderedConversationList.appendChild(conversationItem);
             }
-            else {
-                sessionNum = 0;
-            }
+        } else {
+            sessionNum = 0;
+        }
+    })
+    .catch((error) => {
+        alert("Unsuccessful " + error);
+    });
+
+const conversationList = document.getElementById('conversation-list');
+
+conversationList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        const selectedConversation = event.target.dataset.conversation;
+        getData(parseInt(selectedConversation));
+    }
+});
+
+function getData(i) {
+    chatMessages.innerHTML = '';
+    if (i === 0) {
+        // Initial message
+        const assistantMessage = document.createElement('div');
+        assistantMessage.className = 'assistant-message message';
+        assistantMessage.textContent = "Hi there! How can I assist you today?";
+        chatMessages.appendChild(assistantMessage);
+        sessionID = new Date().getTime();
+        messageNum = 0;
+        return;
+    }
+
+    // Fetch data from the server
+    fetch(`${baseUrl}/conversations/${sessions[i - 1]}`)
+        .then(response => response.json())
+        .then(data => {
+            sessionID = sessions[i - 1];
+            messageNum = data.length;
+            data.forEach((message, index) => {
+                const messageElement = document.createElement('div');
+                messageElement.className = index % 2 === 0 ? 'user-message message' : 'assistant-message message';
+                messageElement.textContent = message;
+                chatMessages.appendChild(messageElement);
+            });
         })
-        .catch((error) => {
-            alert("Unsuccessful " + error);
+        .catch(error => {
+            console.error("Error fetching data:", error);
         });
+}
 
-        const conversationList = document.getElementById('conversation-list');
+function addUserMessage(message) {
+    const userMessage = document.createElement('div');
+    userMessage.className = 'user-message message';
+    userMessage.textContent = message;
+    chatMessages.appendChild(userMessage);
+    userInput.value = '';
 
-        conversationList.addEventListener('click', function (event) {
-            if (event.target.tagName === 'LI') {
-                const selectedConversation = event.target.dataset.conversation;
-                
-               
-                getData(parseInt(selectedConversation));
-            }
-        });
+    storeMessage('user', message, userID);
+}
 
-        function getData(i) {
-            chatMessages.innerHTML = '';
-            if(i == 0) {
-                const assistantMessage = document.createElement('div');
-                assistantMessage.className = 'assistant-message message';
-                assistantMessage.textContent = "Hi there! How can I assist you today?";
-                chatMessages.appendChild(assistantMessage);
-                sessionID = new Date().getTime();
-                messageNum = 0;
-                return;
-            }
-            const dataRef = ref(database);
-            // const sessions = [1696225565878, 1696199129829, 1696199956758, 1696205950086, 1696199956758]; //session key values
-            get(child(dataRef, "Conversations/" + sessions[i-1])).then((snapshot) =>{
-                if(snapshot.exists()) {
-                    console.log(snapshot.val());
-                    sessionID = sessions[i-1];
-                    messageNum = snapshot.val().length
-                    for (let i = 0 ; i < snapshot.val().length ; i++) {
-                        if(i%2 == 0) {
-                            const oldMessage = document.createElement('div');
-                            oldMessage.className = 'user-message message';
-                            oldMessage.textContent = snapshot.val()[i];
-                            chatMessages.appendChild(oldMessage);
-                        } else {
-                            const oldMessage = document.createElement('div');
-                            oldMessage.className = 'assistant-message message';
-                            oldMessage.textContent = snapshot.val()[i];
-                            chatMessages.appendChild(oldMessage);
-                        }
-                    }
+function addAssistantMessage(message) {
+    const assistantMessage = document.createElement('div');
+    assistantMessage.className = 'assistant-message message';
+    assistantMessage.textContent = message;
+    chatMessages.appendChild(assistantMessage);
 
-                    console.log(messageNum + " " + snapshot.val()[0])
-                }
-                else {
-                    alert("No data found");
-                }
+    storeMessage('assistant', message);
+}
+
+function storeMessage(type, message) {
+    if (messageNum === 0) {
+        // Create a new session
+        fetch(`${baseUrl}/sessions/${userID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ sessionID }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                sessions.push(sessionID);
+                sessionNum = sessions.length;
+
+                const conversationItem = document.createElement('li');
+                conversationItem.dataset.conversation = "" + sessionNum;
+                conversationItem.appendChild(document.createTextNode("Conversation: " + sessionNum));
+                unorderedConversationList.appendChild(conversationItem);
+
+                console.log("Session created!");
             })
-            .catch((error) => {
-                alert("Unsuccessful " + error);
+            .catch(error => {
+                console.error("Error creating session:", error);
             });
-        }
+    }
 
-        function addUserMessage(message) {
-            const userMessage = document.createElement('div');
-            userMessage.className = 'user-message message';
-            userMessage.textContent = message;
-            chatMessages.appendChild(userMessage);
-            userInput.value = '';
-
-            storeMessage('user', message); //sends to firebase
-        }
-
-        function addAssistantMessage(message) {
-            const assistantMessage = document.createElement('div');
-            assistantMessage.className = 'assistant-message message';
-            assistantMessage.textContent = message;
-            chatMessages.appendChild(assistantMessage);
-
-            storeMessage('assistant', message);
-        }
-
-        function storeMessage(type, message) {
-            if (messageNum == 0) {
-                const sessionRef = ref(database, "UserToMessages/" + userID + "/" + sessionNum);
-                set(sessionRef, sessionID)
-                .then(() => {
-                    sessions.push(sessionID);
-                    sessionNum = sessionNum + 1;
-                    const conversationItem = document.createElement('li');
-                    conversationItem.dataset.conversation = "" + sessionNum;
-                    conversationItem.appendChild(document.createTextNode("Conversation: " + sessionNum));
-                    unorderedConversationList.appendChild(conversationItem);
-                    console.log("Data stored!");
-                })
-                .catch((error) => {
-                    console.error("Error storing data:", error);
-                });
-            }
-            const messageRef = ref(database, "Conversations/" + sessionID +"/" + messageNum);
-            set(messageRef, message)
-            .then(() => {
-                messageNum = messageNum + 1;
-                console.log("Data stored!");
-            })
-            .catch((error) => {
-                console.error("Error storing data:", error);
-            });
-        }
-
-        userInput.addEventListener('keyup', function (event) {
-            if (event.key === 'Enter') {
-                const userMessage = userInput.value;
-                addUserMessage(userMessage);
-
-                setTimeout(function () {
-                    const response = 'You said: ' + userMessage;
-                    addAssistantMessage(response);
-                }, 1000);
-            }
+    // Store the message in the conversation
+    fetch(`${baseUrl}/conversations/${sessionID}/${messageNum}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, userID }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            messageNum = data.length;
+            console.log("Message stored!");
+        })
+        .catch(error => {
+            console.error("Error storing message:", error);
         });
+}
+
+userInput.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const userMessage = userInput.value;
+        addUserMessage(userMessage);
+
+        setTimeout(function () {
+            const response = 'You said: ' + userMessage;
+            addAssistantMessage(response);
+        }, 1000);
+    }
+});
--- server.js
+++ server.js
@@ -1,8 +1,112 @@
 const express = require('express');
+const { initializeApp } = require('firebase/app');
+const { getDatabase, ref, set, get, child } = require('firebase/database');
+const { MongoClient } = require('mongodb');
+const fetch = require('node-fetch'); // Add this line to use 'fetch' in Node.js
 const app = express();
-const port = 3000;
 
+const port = 3000;
+const cors = require('cors');
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPnrk6AHSsjpiConILj8gYbMGVrrGPw6U",
+  authDomain: "mldl-chatbot.firebaseapp.com",
+  databaseURL: "https://mldl-chatbot-default-rtdb.firebaseio.com",
+  projectId: "mldl-chatbot",
+  storageBucket: "mldl-chatbot.appspot.com",
+  messagingSenderId: "988783282097",
+  appId: "1:988783282097:web:9be4075e02a5e7fed31f90",
+  measurementId: "G-1VQ6B1E6TD"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+
+// MongoDB configuration
+const mongoUrl = 'mongodb+srv://akshatp24:password1234@chatbot.phhnt9t.mongodb.net/?retryWrites=true&w=majority';
+const dbName = 'Chatbot';
+
+async function connectToMongo() {
+  try {
+    const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    console.log('Connected to MongoDB');
+    return client.db(dbName);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+app.use(cors());
+app.use(express.json());
 app.use(express.static(__dirname));
+
+// Create a new session
+app.post('/sessions/:userID', async (req, res) => {
+  const { userID } = req.params;
+  const { sessionID } = req.body;
+
+  const db = await connectToMongo();
+  const sessionCollection = db.collection('sessions');
+
+  try {
+    await sessionCollection.insertOne({
+      userID,
+      sessionID,
+    });
+
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error creating session:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Store a new message in the conversation
+app.post('/conversations/:sessionID/:messageNum', async (req, res) => {
+  const { sessionID, messageNum } = req.params;
+  const { message, userID } = req.body;
+
+  const db = await connectToMongo();
+  const messageCollection = db.collection('messages');
+
+  try {
+    await messageCollection.insertOne({
+      sessionID,
+      message,
+      userID,
+    });
+
+    const messages = await messageCollection.find({ sessionID }).toArray();
+    res.json(messages.map(msg => msg.message));
+  } catch (error) {
+    console.error('Error storing message:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Get all messages for a specific session
+app.get('/conversations/:sessionID', async (req, res) => {
+  const { sessionID } = req.params;
+
+  const db = await connectToMongo();
+  const messageCollection = db.collection('messages');
+
+  try {
+    const messages = await messageCollection.find({ sessionID }).toArray();
+    res.json(messages.map(msg => msg.message));
+  } catch (error) {
+    console.error('Error fetching messages:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
